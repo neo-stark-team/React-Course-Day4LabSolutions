c14efe0ad16c6e2f15e468768e57199e66ac4196
ponmalar <ponmalar@iamneo.ai>
2023-02-01 18:32:11 +0000

    Day11 CEL01

--- CEL03/CEL03-Project Demo/src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../App";
-
-test('test_case01', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/emoji/i);
-  expect(linkElement).toBeInTheDocument();
-});
